cb18402649783d7b43736dfc74176fb61c395347
Rida F'kih <hello@rida.dev>
2022-03-06 01:21:27 -0700

    attach socket to server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-import { app } from "@/modules/app";
+import { app, servers } from "@/modules/app";
+import { io } from "@/modules/socket";
+import { attachServersToSocket } from "@/utils/socket";
+
+attachServersToSocket(io, ...Object.values(servers));
 
 app;
